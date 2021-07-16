71d9fcd8913fc7eb645fcb0df0ce49d1a7577858
Cheshire <jaeho406@gmail.com>
2021-07-16 16:06:15 +0000

    create branch develop

---
+++ src/main/java/org/spring/blog/CheshireApplication.java
@@ -0,0 +1,13 @@
+package org.spring.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheshireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheshireApplication.class, args);
+	}
+
+}
