2bd4bb57d82ce108b5262fef904ddabdfc584ee6
KimJaeho <Nephelai@users.noreply.github.com>
2021-07-17 01:15:42 +0900

    create branch develop (#1)
    21.07.17 Start development

---
+++ src/test/java/org/spring/blog/CheshireApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheshireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
